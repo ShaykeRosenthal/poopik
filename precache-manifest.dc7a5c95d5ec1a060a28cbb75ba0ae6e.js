099c0ce0252f3633914cf2a48642dc36a3fd26a4
shay rosenthal <shayke.rosenthal@gmail.com>
2020-10-04 16:05:02 +0300

    Updates

---
+++ precache-manifest.dc7a5c95d5ec1a060a28cbb75ba0ae6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58daa2e8e2afba67c2b59d1e693482c",
+    "url": "/poopik/index.html"
+  },
+  {
+    "revision": "44add1d64c1fd15adf44",
+    "url": "/poopik/static/css/main.38dd33cd.chunk.css"
+  },
+  {
+    "revision": "6a1bb3abf94a8fb0ae1d",
+    "url": "/poopik/static/js/2.14ab9095.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/poopik/static/js/2.14ab9095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44add1d64c1fd15adf44",
+    "url": "/poopik/static/js/main.30c97fa4.chunk.js"
+  },
+  {
+    "revision": "ca52654c599eca3125d5",
+    "url": "/poopik/static/js/runtime-main.a163c8b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/poopik/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "016d5fefa51fa68f2a6bf84cca36b3a6",
+    "url": "/poopik/static/media/pizza_fresca.016d5fef.jpg"
+  }
+]);
