6cf97d85b61f3bb6f6cdffa9d2da9208a67a2b1e
shay rosenthal <shayke.rosenthal@gmail.com>
2020-10-04 16:04:18 +0300

    deploy to git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import pizzaImg from './img/pizza_fresca.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shayke's pizza</h1>
+      <img src={pizzaImg} alt="" className="src" />
     </div>
   );
 }
