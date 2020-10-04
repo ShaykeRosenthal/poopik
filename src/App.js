6652691086f86a1138bf7e49067fbdeb579cf6f2
shay rosenthal <shayke.rosenthal@gmail.com>
2020-10-04 16:18:10 +0300

    pakaaa

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import pizzaImg from './img/pizza_fresca.jpg';
+import dDuck from './img/dduck.png'
 function App() {
   return (
     <div className="App">
       <h1>Shayke's pizza</h1>
-      <img src={pizzaImg} alt="" className="src" />
+      <img src={dDuck} alt="" className="src" />
     </div>
   );
 }
